79e06b0cb8e241e85e0b75647f098a1c27150fc2
touhidurr <46617994+touhidurr@users.noreply.github.com>
2020-11-07 22:29:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const Discord = require('discord.js');
 const {version} = require('./package.json');
+const {author, help, error, pre, pricelist} = require('./info.json');
 
 const client = new Discord.Client();
 
-const error = 'No such command. See help.\nType `!help`';
-const author = '<@577491473100963840>';
-const pre = '!';
-const pricelist = "";
 var arg;
-
 
 client.once('ready', () => {
     console.log('StoreBot is Ready!');
@@ -16,13 +12,13 @@ client.once('ready', () => {
 
 
 const Cmds = new Map([
-  ["test", "StoreBot is running!"],
-  ["version", "Version " + version],
-  ["about", "StoreBot " + version + "\nAuthor: " + author],
-  //["list", ""],
-  //["pricelist", ""],
-  ["help", "StoreBot v" + version + "\nThe bot is on pre-Alpha testing stage.\nAvailable commands: help, test, version, about."],
-  //["buy", ""]
+  ['test', 'StoreBot is running!'],
+  ['version', 'Version ' + version],
+  ['about', 'StoreBot ' + version + '\nAuthor: ' + author],
+  //['list', ''],
+  //['pricelist', ''],
+  ['help', 'StoreBot v' + version + help],
+  //['buy', '']
 ]);
 
 
@@ -34,7 +30,7 @@ client.on('message', msg => {
     console.log(`${msg.author.username}: ${str}`);
     arg = str.toLowerCase().split(/ +/);
 
-    if(Cmds.has(arg[0])) {
+    if(arg[0] in Cmds) {
       msg.channel.send(Cmds.get(arg[0]));
     } else {
       msg.channel.send(`${msg.author} said: ${str}`);
